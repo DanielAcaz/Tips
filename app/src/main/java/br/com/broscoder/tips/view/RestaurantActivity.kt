3e734da53224486b7308fc152ee7337aabc67b9e
Daniel Acaz <daniel_acaz@hotmail.com>
2019-04-11 00:11:02 -0300

    Created the Table Screen basic

--- app/src/main/java/br/com/broscoder/tips/view/RestaurantActivity.kt
+++ app/src/main/java/br/com/broscoder/tips/view/RestaurantActivity.kt
@@ -1,7 +1,9 @@
 package br.com.broscoder.tips.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.com.broscoder.tips.R
 
 class RestaurantActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class RestaurantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant)
+    }
+
+    fun openTable(view: View) {
+        intent = Intent(this, OrdersActivity::class.java)
+        startActivity(intent)
     }
 }
