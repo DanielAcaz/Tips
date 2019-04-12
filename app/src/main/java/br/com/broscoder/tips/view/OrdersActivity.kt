4af6f36cfad638c3b65ddac110d3c025e534d427
Daniel <daniel.acaz@becommerce.com.br>
2019-04-11 21:08:51 -0300

    Created the friends screen

--- app/src/main/java/br/com/broscoder/tips/view/OrdersActivity.kt
+++ app/src/main/java/br/com/broscoder/tips/view/OrdersActivity.kt
@@ -1,7 +1,9 @@
 package br.com.broscoder.tips.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.com.broscoder.tips.R
 
 class OrdersActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class OrdersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders)
+    }
+
+    fun addFriends(view: View) {
+        intent = Intent(this, FriendsActivity::class.java)
+        startActivity(intent)
     }
 }
